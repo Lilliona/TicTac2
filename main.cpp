9f55dd2db70fb0e117097c38922f0bd90d209c6b
Julia <kammerlander@ztt.fh-worms.de>
2014-03-28 12:50:27 +0100

    Letztes Update befor Neuaufsetzung #18

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Player
 {
 private:
-	int value_of_player;
+	int value_of_player; //nicht player
 public:
 	Player();
-	Player(int value_of_player);
+	//Player(int value_of_player);
 	~Player();
 	int get_value();
 };
@@ -17,10 +18,10 @@ Player::Player()
 {
 
 }
-Player::Player(int value_of_player)
-{
-	this->value_of_player;
-}
+//Player::Player(int value_of_player)
+//{
+//	this->value_of_player;
+//}
 Player::~Player()
 {
 }
@@ -30,23 +31,69 @@ int Player::get_value()
 	return value_of_player;
 }
 
+class Human: public Player
+{
+private:
+
+public:
+	Human();
+	~Human();
+	void returnhuman();
+};
+
+Human::Human()
+{
+}
+
+Human::~Human()
+{
+}
+
+void Human::returnhuman()
+{
+	cout << "Yay, I have brain :D!" << endl;
+}
+
+class Computer: public Player
+{
+private:
+public:
+	Computer();
+	~Computer();
+	void returncomputer();
+};
+
+Computer::Computer()
+{
+}
+
+Computer::~Computer()
+{
+}
+
+void Computer::returncomputer()
+{
+	cout << "Oh no, I don't have brain D:!" << endl;
+}
+
 class Game
 {
 private:
 
 public:
 	Game();
-//	Game(Player *player1,Player *player2);
 	~Game();
+	void turn();
 	Player *player1;
 	Player *player2;
 	int get_player1();
 	int get_player2();
 };
+
 Game::Game()
 {
-	player1=new Player();
-	player2=new Player();
+	//player1=new Player();
+	//player2=new Player();
 }
 //int Game::get_player1()
 //{
@@ -56,15 +103,60 @@ Game::Game()
 //{
 //	return player2->get_value();
 //}
-//
-//Game::Game()
-//{
-//	this->player1=new Player();
-//	this->player2=new Player();
-//}
-
 Game::~Game()
 {
+}
+
+void Game::turn()
+{
+//	cout << "This is a turn!" << endl; 
+//
+//	for (int i=0; i<2;i++)
+//	{
+//		cout << "Please choose if Player" << i+1 << " is human or computer" << endl;
+//		cout << "[H] Human" << endl << "[C] Computer" << endl; 
+//		string chooseplayer;
+//		getline(cin,chooseplayer);
+//
+//		if (chooseplayer=="h" || chooseplayer=="H")
+//		{
+//			cout << "It's a Human!" << endl;
+//			if (i==0)
+//			{
+//				player1=new Human();
+//			}
+//			else
+//			{
+//				player2=new Human();
+//			}
+//
+//		}
+//		else if (chooseplayer=="c" || chooseplayer=="C")
+//		{	
+//			cout << "It's a Computer!" << endl;
+//						if (i==0)
+//			{
+//				player1=new Computer();
+//			}
+//			else
+//			{
+//				player2=new Computer();
+//		/*	}
+//		}
+//		else
+//		{
+//			cout << "That was the wrong character!" << endl;
+//			i=i-1;
+//		}
+//		cout << endl;*/
+//	/*}*/
+//
+//	 player1->returnhuman();
+//	player2->get_value();  nur möglich
+//
+//	
+//
+				
 }
 
 class Controller
@@ -90,13 +182,8 @@ Controller::~Controller()
 int main()
 {
 	Controller *tictactoe=new Controller();
+	tictactoe->game->turn(); 
 
-	cout << "Der Wert von Player1 ist: " << tictactoe->game->get_player1() << endl;
-	cout << "Der Wert von Player2 ist: " << tictactoe->game->get_player2() << endl;
-
-	//delete tictactoe->game->player1;
-	//delete tictactoe->game->player2;
-	//delete tictactoe->game;
 	delete tictactoe;
 	return 0;
 }
